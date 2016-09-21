cda9d74d7732a69688c01f99804c7e1dbde62a75
Zev N. Kronenberg <zevk@e82.grid.gs.washington.edu>
2016-09-21 11:25:41 -0700

    tab was off, throwing an error

--- break_contigs.py
+++ break_contigs.py
@@ -16,33 +16,31 @@ def main():
 
 	lenmap = {}
 	f = FastaReader(args.assembly)
-    for record in f:
-        id = record.id
-        id2seq[id] = record.sequence[0:-10]
+        for record in f:
+                id = record.id
+                id2seq[id] = record.sequence[0:-10]
+                new_seq = {}
 
+                f = open(args.breakpoint,'r')
+                lines = f.readlines()
+                for line in lines:
+                        attrs = line.split()
+                        if len(attrs) == 1:
+                                curr_contig = attrs[0]
+                                seq = id2seq[curr_contig]
+                        else:
+                                start = long(attrs[0])
+                                end = long(attrs[1])
+                                new_id = curr_contig + '_' + attrs[0] + '_' + attrs[1]
+                                new_seq[new_id] = seq[start:end]
+                                lenmap[new_id] = end - start + 1
+                rec_list = []
+                writer = FastaWriter(args.scaffold)
+                for key in new_seq:
+                        writer.writeRecord(key,new_seq[key])
 
-	new_seq = {}
-	f = open(args.breakpoint,'r')
-	lines = f.readlines()
-	for line in lines:
-		attrs = line.split()
-		if len(attrs) == 1:
-			curr_contig = attrs[0]
-			seq = id2seq[curr_contig]
-		else:
-			start = long(attrs[0])
-			end = long(attrs[1])
-			new_id = curr_contig + '_' + attrs[0] + '_' + attrs[1]
-			new_seq[new_id] = seq[start:end]
-			lenmap[new_id] = end - start + 1
-
-	rec_list = []
-	writer = FastaWriter(args.scaffold)
-	for key in new_seq:
-		writer.writeRecord(key,new_seq[key])
-
-	for key in lenmap:
-		lenfile.write(key + "\t" + str(lenmap[key]) + '\n')
+                for key in lenmap:
+                        lenfile.write(key + "\t" + str(lenmap[key]) + '\n')
 
 if __name__ == '__main__':
 	main()
