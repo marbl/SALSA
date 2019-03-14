ce04802f349b97722b5b6b1ab66029d04c2f7220
machinegun <ghuryejs@gry-compute050.core.nhgri.nih.gov>
2019-03-13 21:01:44 -0400

    support to convert salsa output to .hic added

---
+++ alignments2txt.py
@@ -0,0 +1,130 @@
+import os,sys
+import argparse
+import pickle
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-b','--bed',help='Original bed file of alignments')
+parser.add_argument('-a','--agp',help='AGP file for the final scaffolds')
+parser.add_argument('-l','--length',help="Length of input unitigs")
+
+args = parser.parse_args()
+
+
+
+scaffolds_current = {}
+
+with open(args.agp,'r') as f:
+    for line in f:
+        attrs = line.split()
+        if attrs[4] == 'N':
+            continue
+        if attrs[0] not in scaffolds_current:
+            scaffolds_current[attrs[0]] = []
+        contig = attrs[5]
+        if attrs[-1] == '+':
+            scaffolds_current[attrs[0]].append(contig+':B')
+            scaffolds_current[attrs[0]].append(contig+':E')
+        else:
+            scaffolds_current[attrs[0]].append(contig+':E')
+            scaffolds_current[attrs[0]].append(contig+':B')
+
+
+
+#print breakpoints
+scaff_id = 1
+scaffolds_new = {}
+unitig_length = {}
+old2new = {}
+
+def update_bed(expanded_scaffold):
+    contig2scaffold = {}
+    contig2info = {}
+    scaffold_length = {}
+
+    #print re_counts
+
+    for key in expanded_scaffold:
+        path = expanded_scaffold[key]
+        scaffold_length[key] = 0
+        offset = 0
+        for i in xrange(0,len(path)-1,2):
+            contig = path[i].split(':')[0]
+            contig2scaffold[contig] = key
+            ori = path[i].split(':')[1] + path[i+1].split(':')[1]
+            if ori == 'BE':
+                contig2info[contig] = (offset,offset+unitig_length[contig],'FOW')
+            else:
+                contig2info[contig] = (offset,offset+unitig_length[contig],'REV')
+            offset += unitig_length[contig]
+            scaffold_length[key] += unitig_length[contig]
+
+
+    o_lines = ""
+    count = 0
+    prev_line = ""
+    #print contig2scaffold
+    #sys.exit(1)
+    if True:
+        #output = open(args.directory+'/alignment_iteration_'+str(iteration)+'.bed','w')
+        with open(args.bed,'r') as f:
+            olines = ""
+            prev_scaffold = ""
+            for line in f:
+                if prev_line == "":
+                    prev_line = line
+                    continue
+                else:
+                    prev_attrs = prev_line.split()
+                    curr_attrs = line.split()
+                    try:
+                        prev_contig = prev_attrs[0]
+                        curr_contig = curr_attrs[0]
+                        prev_read = prev_attrs[3].split('/')[0]
+                        curr_read = curr_attrs[3].split('/')[0]
+                        first = prev_attrs[3].split('/')[1]
+                        second = curr_attrs[3].split('/')[1]
+                    except:
+                        continue
+                    if prev_contig in contig2scaffold and curr_contig in contig2scaffold:
+                        prev_scaffold = contig2scaffold[prev_contig]
+                        curr_scaffold = contig2scaffold[curr_contig]
+                        if prev_read == curr_read and first == '1' and second == '2':
+                       # if prev_read == curr_read and first == '1' and second == '2':
+                            prev_info = contig2info[prev_contig]
+                            prev_start = int(prev_attrs[1])
+                            prev_end = int(prev_attrs[2])
+                            new_prev_start = prev_start + prev_info[0]
+                            new_prev_end = prev_end + prev_info[0]
+                            olines += "0\t"+prev_scaffold+'\t'+str(new_prev_start)+"\t0\t"
+                            #o_lines += prev_scaffold+'\t'+str(new_prev_start)+'\t'+str(new_prev_end)+'\t'+prev_attrs[3]+'\n'
+                            count += 1
+
+                            curr_info = contig2info[curr_contig]
+                            curr_start = int(curr_attrs[1])
+                            curr_end = int(curr_attrs[2])
+                            new_curr_start = curr_start + curr_info[0]
+                            new_curr_end = curr_end + curr_info[0]
+                            olines += "1\t"+curr_scaffold+'\t'+str(new_curr_start)+"\t1\n"
+                            #o_lines += curr_scaffold+'\t'+str(new_curr_start)+'\t'+str(new_curr_end)+'\t'+curr_attrs[3]+'\n'
+                            count += 1
+                            if count == 1000000:
+                                print olines
+                                #output.write(o_lines)
+                                count = 0
+                                olines = ""
+
+                prev_line = line
+
+            #write remaining lines
+            print olines
+            #output.write(o_lines)
+            #output.close()
+
+with open(args.length,'r') as f:
+    for line in f:
+        attrs = line.split()
+        unitig_length[attrs[0]] = int(attrs[1])
+
+
+update_bed(scaffolds_current)
+
---
+++ convert.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#module load util/coreutils/8.27
+
+JUICER_JAR=/data/projects/phillippy/software/juicer/cpuscripts/juicer_tools_0.7.0.jar
+SALSA_OUT_DIR=$1
+SCRIPT_PATH=$BASH_SOURCE
+SCRIPT_PATH=`dirname $SCRIPT_PATH`
+
+samtools faidx ${SALSA_OUT_DIR}/scaffolds_FINAL.fasta
+
+cut -f 1,2 ${SALSA_OUT_DIR}/scaffolds_FINAL.fasta.fai > ${SALSA_OUT_DIR}/chromosome_sizes.tsv
+
+python ${SCRIPT_PATH}/alignments2txt.py -b ${SALSA_OUT_DIR}/alignment_iteration_1.bed  -a ${SALSA_OUT_DIR}scaffolds_FINAL.agp -l ${SALSA_OUT_DIR}/scaffold_length_iteration_1 > ${SALSA_OUT_DIR}/alignments.txt
+
+awk '{if ($2 > $6) {print $1"\t"$6"\t"$7"\t"$8"\t"$5"\t"$2"\t"$3"\t"$4} else {print}}'  ${SALSA_OUT_DIR}/alignments.txt | sort -k2,2d -k6,6d -T $PWD --parallel=16 | awk 'NF'  > ${SALSA_OUT_DIR}/alignments_sorted.txt
+
+java -jar ${JUICER_JAR} pre ${SALSA_OUT_DIR}/alignments_sorted.txt ${SALSA_OUT_DIR}/salsa_scaffolds.hic ${SALSA_OUT_DIR}/chromosome_sizes.tsv
+
+
